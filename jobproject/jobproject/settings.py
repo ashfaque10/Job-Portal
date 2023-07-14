3c1023bc66f0c27f1cb865b028f78e208fbe3608
ashfaque10 <124551893+ashfaque10@users.noreply.github.com>
2023-07-14 09:10:13 +0530

    Update settings.py

--- jobproject/jobproject/settings.py
+++ jobproject/jobproject/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAI_PORT=587
-EMAIL_HOST_USER='ashuashfaque213@gmail.com'
-EMAIL_HOST_PASSWORD='wvootkclszrhetms'
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
